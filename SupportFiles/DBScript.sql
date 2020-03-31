953c8bf11c9747eb885cb093e6821115e5e66d3a
Chaiminda Chandrasekara <chaminda@xamariners.com>
2020-03-31 20:54:59 +0800

    add support fil and instruction files for deploy

---
+++ SupportFiles/DBScript.sql
@@ -0,0 +1,27 @@
+﻿IF OBJECT_ID(N'[__EFMigrationsHistory]') IS NULL
+BEGIN
+    CREATE TABLE [__EFMigrationsHistory] (
+        [MigrationId] nvarchar(150) NOT NULL,
+        [ProductVersion] nvarchar(32) NOT NULL,
+        CONSTRAINT [PK___EFMigrationsHistory] PRIMARY KEY ([MigrationId])
+    );
+END;
+
+GO
+
+CREATE TABLE [Employees] (
+    [EmployeeId] int NOT NULL IDENTITY,
+    [FullName] nvarchar(250) NOT NULL,
+    [EmpCode] varchar(10) NULL,
+    [Position] varchar(100) NULL,
+    [OfficeLocation] varchar(100) NULL,
+    CONSTRAINT [PK_Employees] PRIMARY KEY ([EmployeeId])
+);
+
+GO
+
+INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+VALUES (N'20181223091420_InitialCreate', N'2.2.6-servicing-10079');
+
+GO
+
---
+++ SupportFiles/UpdateDB.ps1
@@ -0,0 +1,34 @@
+﻿param(
+    [Parameter(Mandatory=$true)]
+    [string] $connectionString,
+    [Parameter(Mandatory=$true)]
+    [string] $scriptFileName
+)
+
+Write-Host "Reading $PSScriptRoot\$scriptFileName"
+
+$query = [IO.File]::ReadAllText("$PSScriptRoot\$scriptFileName")
+
+$batches = $query -split "GO\r\n"
+
+Write-Host "Connection to DB for update with connection info: $connectionString"
+
+$connection = New-Object -TypeName System.Data.SqlClient.SqlConnection($connectionString)
+$connection.Open()
+
+foreach($batch in $batches)
+{
+    if ($batch.Trim() -ne ""){
+
+        Write-Verbose "Executing... $batch"
+
+        $SqlCmd = New-Object System.Data.SqlClient.SqlCommand
+        $SqlCmd.CommandText = $batch
+        $SqlCmd.Connection = $connection
+        $SqlCmd.ExecuteNonQuery()
+    }
+}
+
+Write-Host "Update completed successfully. Closing DB connection..."
+
+$connection.Close()
